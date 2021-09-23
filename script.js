0da63e7b721d058760dee01e6f2d48b433a06545
nikolaswise <nikolaswise@gmail.com>
2021-09-23 12:01:45 -0700

    wrap up newsletter

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 const submitSignup = (e) => {
   e.preventDefault()
   let successNode = e.target.querySelector('.js-form-success')
   let errorNode = e.target.querySelector('.js-form-error')
+  let invalidNode = document.querySelector('.js-form-invalid')
   let inputNode = e.target.querySelector('input')
   let buttonNode = e.target.querySelector('button')
   let email = e.target.firstElementChild.value
 
-  fetch(`https://www.smugmug.com/newsletter/flickr-foundation?email=${email}`, {
-    method: 'GET',
-    mode: 'cors',
-  })
-    .then(r => r.json())
-    .then(r => {
-      inputNode.classList.add('is-hidden')
-      buttonNode.classList.add('is-hidden')
-      successNode.classList.add('is-active')
+  if (validateEmail(email)) {
+    fetch(`https://www.smugmug.com/newsletter/flickr-foundation?email=${email}`, {
+      method: 'GET',
+      mode: 'cors',
     })
-    .catch(e => {
-      inputNode.classList.add('is-hidden')
-      buttonNode.classList.add('is-hidden')
-      successNode.classList.add('is-active')
-    })
+      .then(r => r.json())
+      .then(r => {
+        inputNode.classList.add('is-hidden')
+        buttonNode.classList.add('is-hidden')
+        successNode.classList.add('is-active')
+        invalidNode.classList.remove('is-active')
+      })
+      .catch(e => {
+        inputNode.classList.add('is-hidden')
+        buttonNode.classList.add('is-hidden')
+        successNode.classList.add('is-active')
+        invalidNode.classList.remove('is-active')
+      })
+  } else {
+    invalidNode.classList.add('is-active')
+  }
 }
 
 const newsletterSignup = () => {
